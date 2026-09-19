ad2de8936dccdb05a26156153364f8c775c8f698
Kolja Sam <github@koljapluemer.com>
2026-09-19 16:26:14 +0200

    cleanup

--- flutter_app/android/app/src/main/kotlin/com/example/sentences/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sentences
-
-import com.ryanheise.audioservice.AudioServiceActivity
-
-// AudioServiceActivity (a FlutterActivity subclass) is required by
-// just_audio_background so playback survives with the screen off.
-class MainActivity : AudioServiceActivity()
